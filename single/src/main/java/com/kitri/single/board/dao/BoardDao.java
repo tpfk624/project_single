22b21bb11dbf3a883aba809dc8e39f166fffffaa
kwon-beom-jun <qjawns0618@naver.com>
2019-07-04 15:50:00 +0900

    2019-07-04 KBJ update!!!

--- single/src/main/java/com/kitri/single/board/dao/BoardDao.java
+++ single/src/main/java/com/kitri/single/board/dao/BoardDao.java
@@ -1,12 +1,14 @@
 package com.kitri.single.board.dao;
 
+import java.util.List;
+
 import com.kitri.single.board.model.BoardDto;;
 
 
 public interface BoardDao {
 
 	int writeArticle(BoardDto boardDto);
-	int writeArticlehashtag(BoardDto boardDto);
+	int writeArticlehashtag(String hashtag);
 	
 	BoardDto viewArticle(int boardNum);
 	
